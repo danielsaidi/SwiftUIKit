2bd28c371a0a4db0e9f9c23e75ef7259fade7d3d
Daniel Saidi <daniel.saidi@gmail.com>
2020-03-04 23:36:48 +0100

    Begin adding demo project

---
+++ SwiftUIKitDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIKitDemo
+//
+//  Created by Daniel Saidi on 2020-03-04.
+//  Copyright © 2020 Daniel Saidi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
