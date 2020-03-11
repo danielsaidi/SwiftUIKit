577feeb0057a9ff176ef1c970873dbd8410fa2a5
Daniel Saidi <daniel.saidi@gmail.com>
2020-03-11 12:34:36 +0100

    Simplify toast demo setup

--- SwiftUIKitDemo/Toast/ToastScreen.swift
+++ SwiftUIKitDemo/Toast/ToastScreen.swift
@@ -19,15 +19,9 @@ struct ToastScreen: View, DemoList {
     var body: some View {
         ScrollView {
             VStack(spacing: listSpacing) {
-                ToastCell(
-                    title: "Tap to show a text toast toast",
-                    color: .red
-                ).onTapGesture(perform: showTextToast)
-
-                ToastCell(
-                    title: "Tap to show a german flag toast",
-                    color: .yellow
-                ).onTapGesture(perform: showViewToast)
+                item(title: "Tap me to show a text toast", color: .red, action: showTextToast)
+                item(title: "Tap me to show a flag toast", color: .green, action: showFlagToast)
+                item(title: "Tap me to show an emoji toast", color: .blue, action: showEmojiToast)
             }
         }.toast(
             isPresented: $isToastActive,
@@ -39,16 +33,25 @@ struct ToastScreen: View, DemoList {
 
 private extension ToastScreen {
     
-    func showTextToast() {
-        toastView = Text("This is a text toast").any()
+    func item(title: String, color: Color, action: @escaping () -> Void) -> some View {
+        DemoListItem(title: title, content: color, effect: { $0 })
+            .onTapGesture(perform: action)
     }
     
-    func showViewToast() {
+    func showEmojiToast() {
+        toastView = Text("😀❤️👏👍🎉🎊🍾").any()
+    }
+    
+    func showFlagToast() {
         toastView = VStack(spacing: 0) {
             Color.black.frame(width: 200, height: 50)
             Color.red.frame(width: 200, height: 50)
             Color.yellow.frame(width: 200, height: 50)
         }.any()
+    }
+    
+    func showTextToast() {
+        toastView = Text("This is a text toast").any()
     }
 }
 
