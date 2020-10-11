0f923e53302c13be8f7cce05a1c26dc6352fec81
Daniel Saidi <daniel.saidi@gmail.com>
2020-10-11 10:46:58 +0200

    Add new MultiPageView

---
+++ Sources/SwiftUIKit/Views/MultiPageView.swift
@@ -0,0 +1,47 @@
+//
+//  MultiPageView.swift
+//  SwiftUIKit
+//
+//  Created by Daniel Saidi on 2020-10-11.
+//  Copyright © 2020 Daniel Saidi. All rights reserved.
+//
+//  Credits to @pawello2222
+//  https://stackoverflow.com/questions/58388071/how-can-i-implement-pageview-in-swiftui/63159912
+//
+
+import SwiftUI
+
+@available(iOS 14.0, *)
+public struct MultiPageView: View {
+    
+    public init<PageType: View>(
+        pages: [PageType],
+        indexDisplayMode: PageTabViewStyle.IndexDisplayMode = .automatic,
+        currentPageIndex: Binding<Int>) {
+        self.pages = pages.map { $0.any() }
+        self.indexDisplayMode = indexDisplayMode
+        self.currentPageIndex = currentPageIndex
+    }
+    
+    public init(
+        pages: [AnyView],
+        indexDisplayMode: PageTabViewStyle.IndexDisplayMode = .automatic,
+        currentPageIndex: Binding<Int>) {
+        self.pages = pages
+        self.indexDisplayMode = indexDisplayMode
+        self.currentPageIndex = currentPageIndex
+    }
+    
+    private let pages: [AnyView]
+    private let indexDisplayMode: PageTabViewStyle.IndexDisplayMode
+    private var currentPageIndex: Binding<Int>
+    
+    public var body: some View {
+        TabView(selection: currentPageIndex) {
+            ForEach(Array(pages.enumerated()), id: \.offset) {
+                $0.element.tag($0.offset)
+            }
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: indexDisplayMode))
+    }
+}
