00e94b136e43dc791ab268e252cf4e69dcceedfb
Daniel Saidi <daniel.saidi@gmail.com>
2020-11-10 23:59:27 +0100

    Add a lazy load action to the collection view

--- Sources/SwiftUIKit/Views/Collections/CollectionView.swift
+++ Sources/SwiftUIKit/Views/Collections/CollectionView.swift
@@ -17,7 +17,13 @@ import SwiftUI
  are currently these following layouts to choose from:
  
  * `.catalog` creates a vertical list with horizontal rows.
- * `.grid` creates a vertical grid with x items per grid row. 
+ * `.grid` creates a vertical grid with x items per grid row.
+ 
+ The view can trigger the provided `lazyLoadAction` when the
+ last **row** in a multi-row collection or the last **item**
+ in a single row collection is displayed. This action should
+ fetch more content and append it to the list that generates
+ the provided `rows`.
  
  Credits to `@defagos`, who created this as part of his work
  to bring amazing, performant collection views to `SwiftUI`:
@@ -34,12 +40,15 @@ public struct CollectionView<Section: Hashable, Item: Hashable, Cell: View, Supp
     public init(
         rows: [CollectionViewRow<Section, Item>],
         layout: CollectionViewLayout,
+        lazyLoadAction: @escaping () -> Void = {},
         @ViewBuilder cell: @escaping (IndexPath, Item) -> Cell,
         @ViewBuilder supplementaryView: @escaping (String, IndexPath) -> SupplementaryView) {
-        self.cell = cell
-        self.rows = rows
-        self.sectionLayoutProvider = layout.sectionLayoutProvider
-        self.supplementaryView = supplementaryView
+        self.init(
+            rows: rows,
+            sectionLayoutProvider: layout.sectionLayoutProvider,
+            lazyLoadAction: lazyLoadAction,
+            cell: cell,
+            supplementaryView: supplementaryView)
     }
     
     /**
@@ -48,9 +57,11 @@ public struct CollectionView<Section: Hashable, Item: Hashable, Cell: View, Supp
     public init(
         rows: [CollectionViewRow<Section, Item>],
         sectionLayoutProvider: @escaping (Int, NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection,
+        lazyLoadAction: @escaping () -> Void = {},
         @ViewBuilder cell: @escaping (IndexPath, Item) -> Cell,
         @ViewBuilder supplementaryView: @escaping (String, IndexPath) -> SupplementaryView) {
         self.cell = cell
+        self.lazyLoadAction = lazyLoadAction
         self.rows = rows
         self.sectionLayoutProvider = sectionLayoutProvider
         self.supplementaryView = supplementaryView
@@ -62,6 +73,7 @@ public struct CollectionView<Section: Hashable, Item: Hashable, Cell: View, Supp
     public let rows: [CollectionViewRow<Section, Item>]
     
     private let cell: (IndexPath, Item) -> Cell
+    private let lazyLoadAction: () -> Void
     private let sectionLayoutProvider: (Int, NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection
     private let supplementaryView: (String, IndexPath) -> SupplementaryView
     
@@ -72,7 +84,7 @@ public struct CollectionView<Section: Hashable, Item: Hashable, Cell: View, Supp
     // MARK: - Public Functions
     
     public func makeCoordinator() -> Coordinator {
-        Coordinator()
+        Coordinator(lazyLoadFunction: lazyLoadAction, lazyLoadTrigger: shouldTriggerLazyLoad)
     }
     
     public func makeUIView(context: Context) -> UICollectionView {
@@ -114,6 +126,17 @@ private extension CollectionView {
         UICollectionViewCompositionalLayout { sectionIndex, layoutEnvironment in
             context.coordinator.sectionLayoutProvider?(sectionIndex, layoutEnvironment)
         }
+    }
+    
+    func shouldTriggerLazyLoad(forItemAt indexPath: IndexPath) -> Bool {
+        let rowCount = rows.count
+        let hasManySections = rowCount > 1
+        let isLastRow = indexPath.section == rowCount - 1
+        let isFirstItem = indexPath.row == 0
+        if hasManySections { return isLastRow && isFirstItem }
+        let itemCount = rows.last?.items.count ?? 0
+        let isLastItem = indexPath.row == itemCount - 1
+        return isLastItem
     }
     
     func setupSupplementaryView(for context: Context, dataSource: Coordinator.DataSource) {
@@ -162,7 +185,17 @@ public extension CollectionView {
     
     class Coordinator: NSObject, UICollectionViewDelegate {
         
+        init(
+            lazyLoadFunction: @escaping () -> Void,
+            lazyLoadTrigger: @escaping (IndexPath) -> Bool) {
+            self.lazyLoadFunction = lazyLoadFunction
+            self.lazyLoadTrigger = lazyLoadTrigger
+        }
+        
         typealias DataSource = UICollectionViewDiffableDataSource<Section, Item>
+        
+        private let lazyLoadFunction: () -> Void
+        private let lazyLoadTrigger: (IndexPath) -> Bool
         
         var dataSource: DataSource? = nil
         var sectionLayoutProvider: ((Int, NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection)?
@@ -172,6 +205,11 @@ public extension CollectionView {
         
         public func collectionView(_ collectionView: UICollectionView, canFocusItemAt indexPath: IndexPath) -> Bool {
             isFocusable
+        }
+        
+        public func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+            guard lazyLoadTrigger(indexPath) else { return }
+            lazyLoadFunction()
         }
     }
 }
