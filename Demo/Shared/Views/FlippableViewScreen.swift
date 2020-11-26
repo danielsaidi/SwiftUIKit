44680b2df6f6664294609cfcab980c3ec68658d6
Daniel Saidi <daniel.saidi@gmail.com>
2020-11-26 23:26:41 +0100

    Improve flippable view and add new demo

--- Demo/Shared/Views/FlippableViewScreen.swift
+++ Demo/Shared/Views/FlippableViewScreen.swift
@@ -13,8 +13,7 @@ struct FlippableViewScreen: View {
     @State private var isFlipped: Bool = false
     
     var body: some View {
-        SimpleFlipper(
-            flipped: $isFlipped,
+        FlippableView(
             front: side(.blue, "Flip me!"),
             back: side(.red, "Flip me back!")
         )
@@ -37,38 +36,4 @@ struct FlippableViewScreen_Previews: PreviewProvider {
     static var previews: some View {
         FlippableViewScreen()
     }
-}
-struct SimpleFlipper<Front: View, Back: View> : View {
-    
-    @Binding var flipped: Bool
-    var front: Front
-    var back: Back
-
-      var body: some View {
-
-            let flipDegrees = flipped ? 180.0 : 0
-
-            return VStack{
-                  Spacer()
-
-                  ZStack() {
-                        front.flipRotate(flipDegrees).opacity(flipped ? 0.0 : 1.0)
-                        back.flipRotate(-180 + flipDegrees).opacity(flipped ? 1.0 : 0.0)
-                  }
-                  .animation(.easeInOut(duration: 0.5))
-                  .onTapGesture { self.flipped.toggle() }
-                  Spacer()
-            }
-      }
-}
-
-extension View {
-
-      func flipRotate(_ degrees : Double) -> some View {
-            return rotation3DEffect(Angle(degrees: degrees), axis: (x: 0.0, y: 1.0, z: 0.0))
-      }
-
-    func placedOnCard<Card: View>(_ card: Card) -> some View {
-            return padding(5).frame(width: 250, height: 150, alignment: .center).background(card)
-      }
 }
--- Sources/SwiftUIKit/Views/Flippable/FlippableView.swift
+++ Sources/SwiftUIKit/Views/Flippable/FlippableView.swift
@@ -6,65 +6,103 @@
 //  Copyright © 2020 Daniel Saidi. All rights reserved.
 //
 
-#if os(iOS)
 import SwiftUI
 
 /**
- This view consists of a front and a back view, where the amd can be flipped by either tapping the view or.
+ This view has a front and a back view and can be flipped to
+ show the other side when it's tapped, swiped or both.
+ 
+ For now, it only supports horizontal flip, since flipping a
+ view both horizontally and vertically can mess it up and is
+ not good from a usability point of view, since the view can
+ become upside down.
  */
-public struct FlippableView<Front: View, Back: View>: View, Flippable {
+public struct FlippableView<Front: View, Back: View>: View {
     
-    public init(front: Front, back: Back) {
-        self.frontView = front
-        self.backView = back
-    }
+    var front: Front
+    var back: Back
+    var isTapEnabled: Bool = true
+    var isSwipeEnabled: Bool = true
     
-    private let frontView: Front
-    private let backView: Back
+    private let flipDegrees = 180.0
     
     @State private var isFlipped = false
-    @State private var rotationX = 0.0
-    @State private var rotationY = 0.0
+    @State private var isFlippedWithTap = false
+    @State private var yDegrees: Double = 0
+    @State private var xDegrees: Double = 0
     
-    public func flip(_ direction: FlipDirection) {
-        flipHalfways(direction: direction)
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-            self.isFlipped.toggle()
-            self.flipHalfways(direction: direction)
-        }
-    }
-    
-    public func flipHalfways(direction: FlipDirection) {
-        withAnimation {
-            switch direction {
-            case .left: self.rotationY -= 90
-            case .right: self.rotationY += 90
-            case .up: self.rotationX += 90
-            case .down: self.rotationX -= 90
-            }
-        }
+    public enum Direction {
+        case forward, backward
+        var multiplier: CGFloat { self == .forward ? 1 : -1 }
     }
     
     public var body: some View {
-        content
-            .rotation3DEffect(Angle(degrees: self.rotationX), axis: (x: 1, y: 0, z: 0))
-            .rotation3DEffect(Angle(degrees: self.rotationY), axis: (x: 0, y: 1, z: 0))
+        VStack{
+            Spacer()
+            ZStack() {
+                front.flipRotateY(degrees: yDegrees)
+                    .opacity(isFlipped ? 0.0 : 1.0)
+                back.flipRotateY(degrees: -180 + yDegrees)
+                    .opacity(isFlipped ? 1.0 : 0.0)
+            }
+            .animation(.easeInOut(duration: 0.5))
             .onSwipeGesture(
-                up: { self.flip(.up) },
-                left: { self.flip(.left) },
-                right: { self.flip(.right) },
-                down: { self.flip(.down) }
-            )
-            .onTapGesture { self.flip(.right) }
-    }
-    
-    @ViewBuilder
-    private var content: some View {
-        if isFlipped {
-            backView
-        } else {
-            frontView
+                left: flipWithSwipeLeft,
+                right: flipWithSwipeRight)
+            .onTapGesture(perform: flipWithTap)
+            Spacer()
         }
     }
 }
-#endif
+
+private extension FlippableView {
+    
+    func flip(_ direction: FlipDirection) {
+        isFlipped.toggle()
+        let degrees = flipDegrees * multiplier(for: direction)
+        switch direction {
+        case .left, .right: yDegrees += degrees
+        default: break
+        }
+    }
+    
+    func flipWithSwipeLeft() {
+        guard isSwipeEnabled else { return }
+        isFlippedWithTap = false
+        flip(.left)
+    }
+    
+    func flipWithSwipeRight() {
+        guard isSwipeEnabled else { return }
+        isFlippedWithTap = false
+        flip(.right)
+    }
+    
+    func flipWithTap() {
+        guard isTapEnabled else { return }
+        flip(isFlippedWithTap ? .left : .right)
+        isFlippedWithTap.toggle()
+    }
+    
+    func multiplier(for direction: FlipDirection) -> Double {
+        switch direction {
+        case .up, .right: return 1
+        case .down, .left: return -1
+        }
+    }
+}
+
+private extension View {
+    
+    func flipRotateX(degrees: Double) -> some View {
+        flipRotate(x: 1, degrees: degrees)
+    }
+    
+    func flipRotateY(degrees: Double) -> some View {
+        flipRotate(y: 1, degrees: degrees)
+    }
+    
+    func flipRotate(x: CGFloat = 0, y: CGFloat = 0, degrees: Double) -> some View {
+        rotation3DEffect(Angle(degrees: degrees), axis: (x: x, y: y, z: 0.0))
+    }
+}
