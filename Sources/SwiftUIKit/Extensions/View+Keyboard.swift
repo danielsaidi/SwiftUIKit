0df5b5ff4a177bac683bdb6c586a0fb3f305fc99
Daniel Saidi <daniel.saidi@gmail.com>
2020-12-18 00:32:15 +0100

    Make keyboard extension public

--- Sources/SwiftUIKit/Extensions/View+Keyboard.swift
+++ Sources/SwiftUIKit/Extensions/View+Keyboard.swift
@@ -10,7 +10,7 @@
 import UIKit
 import SwiftUI
 
-extension View {
+public extension View {
     
     func hideKeyboard() {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
