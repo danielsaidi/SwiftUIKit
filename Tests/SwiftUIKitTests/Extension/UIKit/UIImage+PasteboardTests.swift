cbc881afaf7c4cdc618f7b9562380e2e1e1a4371
Daniel Saidi <daniel.saidi@gmail.com>
2021-02-05 08:11:59 +0100

    Update to MockingKit 0.9

--- Tests/SwiftUIKitTests/Extension/UIKit/UIImage+PasteboardTests.swift
+++ Tests/SwiftUIKitTests/Extension/UIKit/UIImage+PasteboardTests.swift
@@ -27,7 +27,7 @@ class UIImage_PasteboardTests: QuickSpec {
             
             it("ignores image without png data") {
                 let result = UIImage().copyToPasteboard(pasteboard)
-                let invokes = pasteboard.invokations(of: pasteboard.setDataRef)
+                let invokes = pasteboard.calls(to: pasteboard.setDataRef)
                 expect(result).to(beFalse())
                 expect(invokes.count).to(equal(0))
             }
@@ -35,7 +35,7 @@ class UIImage_PasteboardTests: QuickSpec {
             it("sets correctly formatted data") {
                 let image = UIImage().resized(to: CGSize(width: 1, height: 1))!
                 let result = image.copyToPasteboard(pasteboard)
-                let invokes = pasteboard.invokations(of: pasteboard.setDataRef)
+                let invokes = pasteboard.calls(to: pasteboard.setDataRef)
                 expect(result).to(beTrue())
                 expect(invokes.count).to(equal(1))
                 expect(invokes[0].arguments.0).toNot(beNil())
