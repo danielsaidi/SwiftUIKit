8a88a7c2e6ebea1403c6dea497b6f2eee7afe0b9
Daniel Saidi <daniel.saidi@gmail.com>
2021-09-09 17:32:48 +0200

    Update menu list toggle to also support an icon

--- Sources/SwiftUIKit/Views/MenuList/MenuListToggle.swift
+++ Sources/SwiftUIKit/Views/MenuList/MenuListToggle.swift
@@ -12,20 +12,61 @@ import SwiftUI
  A menu list toggle is a specialized toggle that takes up as
  much vertical space as a menu list text view.
  */
-public struct MenuListToggle: View {
+public struct MenuListToggle<Icon: View>: View {
     
     public init(
-        _ title: String,
+        iconView: Icon,
+        title: String,
         isOn: Binding<Bool>) {
+        self.icon = iconView
         self.title = title
         self.isOn = isOn
     }
     
+    private let icon: Icon?
     private let title: String
     private let isOn: Binding<Bool>
     
     public var body: some View {
-        Toggle(title, isOn: isOn)
-            .padding(.vertical, 8)
+        MenuListItemStack {
+            if let icon = icon {
+                MenuListIcon(icon)
+            }
+            Toggle(title, isOn: isOn)
+                .padding(.vertical, 8)
+        }
+    }
+}
+
+extension MenuListToggle where Icon == Image {
+    
+    public init(
+        _ title: String,
+        isOn: Binding<Bool>) {
+        self.icon = nil
+        self.title = title
+        self.isOn = isOn
+    }
+    
+    public init(
+        icon: Image,
+        title: String,
+        isOn: Binding<Bool>) {
+        self.icon = icon
+        self.title = title
+        self.isOn = isOn
+    }
+}
+
+struct MenuListToggle_Previews: PreviewProvider {
+    
+    static let icon = Image(systemName: "trash")
+    
+    static var previews: some View {
+        List {
+            MenuListToggle("foo", isOn: .constant(true))
+            MenuListToggle(icon: icon, title: "foo", isOn: .constant(true))
+            MenuListToggle(iconView: Color.red, title: "foo", isOn: .constant(true))
+        }
     }
 }
