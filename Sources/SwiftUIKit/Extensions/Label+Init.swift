5997e9cd4d180783fe8b138e8b0970b321f9362d
Daniel Saidi <daniel.saidi@gmail.com>
2021-11-10 11:01:10 +0100

    Add a new Label initializer

---
+++ Sources/SwiftUIKit/Extensions/Label+Init.swift
@@ -0,0 +1,20 @@
+//
+//  Label+Init.swift
+//  SwiftUIKit
+//
+//  Created by Daniel Saidi on 2021-11-10.
+//  Copyright © 2021 Daniel Saidi. All rights reserved.
+//
+
+import SwiftUI
+
+@available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
+public extension Label where Icon == Image, Title == Text {
+    
+    /**
+     Create a label with a string and an image.
+     */
+    init(_ text: String, image: Image) {
+        self.init(title: { Text(text) }, icon: { image })
+    }
+}
