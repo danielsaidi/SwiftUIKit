3ae1e10e3333db9ccf400b3562c451b7b306a959
Daniel Saidi <daniel.saidi@gmail.com>
2022-10-13 18:53:29 +0200

    Adjust circular progress bar and fix so it isn't greedy

--- Sources/SwiftUIKit/Views/Progress/CircularProgressBar.swift
+++ Sources/SwiftUIKit/Views/Progress/CircularProgressBar.swift
@@ -9,62 +9,96 @@
 import SwiftUI
 
 /**
- This view displays a percentual progress as a circle stroke,
- with the progress text in the center of the circle.
+ This view displays progress as a circle progress view, with
+ the progress text in the center of the circle.
  
- `startAngle` is the number of degrees from the circle's top
- from which the arc starts. The default `0` value means that
- the arc starts from the top, `90` from the right etc.
+ ``startAngle`` is the number of degrees from the circle top,
+ going clockwise. `0` means that the arc starts from the top,
+ `90` from the right etc.
 
- Override the static `standard` style instance to change how
- these views look when you don't provide a `style` in `init`.
- The standard colors are not adaptive and will not adjust to
- e.g. dark mode.
+ The view can be styled with a ``Style``. There are a couple
+ of built-in styles to choose from. You can also create your
+ own styles like this:
 
- `TODO:` Make ``CircularProgressBarStyle`` not use `AnyView`.
+ ```
+ extension CircularProgressBar.Style {
+
+     static var swedish: CircularProgressBar.Style = {
+         var style = CircularProgressBar.Style.standard
+         style.backgroundColor = .blue
+         style.strokeColor = .yellow
+         style.strokeWidth = 5
+         style.progressColor = .yellow
+         style.progressWidth = 15
+         style.progressModifier = { $0.shadow(.elevated).any() }
+         return style
+     }()
+ }
+ ```
+
+ and use it like this:
+
+ ```
+ CircularProgressBar(
+     progress: 0.8,
+     style: .swedish)
+ ```
+
+ You can modify the ``Style/standard`` style to change how a
+ view will look by default, if you don't provide a style.
  */
 public struct CircularProgressBar: View {
-    
+
+    /**
+     Create a circular progress bar.
+
+     - Parameters:
+       - progress: The percentage progress, from `0` to `1`.
+       - decimals: The number of decimals to display, by default `0`.
+       - startAngle: The start angle in degrees, by default `0`.
+       - style: The style to apply, by default ``Style/standard``.
+     */
     public init(
-        progress: Binding<Double>,
+        progress: Double,
         decimals: Int = 0,
         startAngle: Double = 0,
-        style: CircularProgressBarStyle = .standard
+        style: Style = .standard
     ) {
-        _progress = progress
+        self.progress = progress
         self.decimals = decimals
         self.startAngle = startAngle
         self.style = style
     }
-    
-    private let decimals: Int
-    private let startAngle: Double
-    private let style: CircularProgressBarStyle
-    
-    @Binding
-    private var progress: Double
+
+    /// The percentage progress.
+    public let progress: Double
+
+    /// The number of decimals to display.
+    public let decimals: Int
+
+    /// The start angle in degrees.
+    public let startAngle: Double
+
+    /// The style to apply.
+    public let style: CircularProgressBar.Style
     
     public var body: some View {
-        ZStack {
-            baseCircle
-            progressCircle
-            if style.showTitle {
-                progressText
-            }
-        }
+        baseCircle
+            .overlay(progressCircle)
+            .overlay(progressText)
     }
+}
+
+private extension CircularProgressBar {
     
-    
-    // MARK: - Private Logic (extension makes previews fail)
-    
-    private var baseCircle: some View {
+    var baseCircle: some View {
         Circle()
             .stroke(lineWidth: style.strokeWidth)
             .foregroundColor(style.strokeColor)
             .background(Circle().fill(style.backgroundColor))
     }
     
-    private var progressCircle: some View {
+    var progressCircle: some View {
         style.progressModifier(
             Circle()
                 .trim(from: 0.0, to: CGFloat(progress))
@@ -75,52 +109,117 @@ public struct CircularProgressBar: View {
                 .any()
         )
     }
-    
-    private var progressText: some View {
-        Text("\(progressValue, specifier: "%.\(decimals)f")%")
-            .lineLimit(1)
-            .font(style.titleFont)
-            .foregroundColor(style.titleColor)
+
+    @ViewBuilder
+    var progressText: some View {
+        if style.showTitle {
+            style.titleModifier(
+                Text("\(progressValue, specifier: "%.\(decimals)f")%")
+                    .lineLimit(1)
+                    .font(style.titleFont)
+                    .foregroundColor(style.titleColor)
+                    .any()
+            )
+        }
     }
     
-    private var progressValue: Double {
+    var progressValue: Double {
         max(min(progress * 100, 100), 0)
+    }
+}
+
+public extension CircularProgressBar {
+
+    /**
+     This style can be used to style ``CircularProgressBar``.
+     */
+    struct Style {
+
+        public init(
+            backgroundColor: Color = Color.black.opacity(0.9),
+            strokeColor: Color = .black,
+            strokeWidth: CGFloat = 2,
+            progressColor: Color = .white,
+            progressWidth: CGFloat = 8,
+            progressModifier: @escaping ViewModifier = { $0 },
+            showTitle: Bool = true,
+            titleColor: Color = .white,
+            titleFont: Font = Font.body.bold(),
+            titleModifier: @escaping ViewModifier = { $0 }
+        ) {
+            self.backgroundColor = backgroundColor
+            self.strokeColor = strokeColor
+            self.strokeWidth = strokeWidth
+            self.progressColor = progressColor
+            self.progressWidth = progressWidth
+            self.progressModifier = progressModifier
+            self.showTitle = showTitle
+            self.titleColor = titleColor
+            self.titleFont = titleFont
+            self.titleModifier = titleModifier
+        }
+
+        public typealias ViewModifier = (AnyView) -> AnyView
+
+        public var backgroundColor: Color
+        public var strokeColor: Color
+        public var strokeWidth: CGFloat
+        public var progressColor: Color
+        public var progressWidth: CGFloat
+        public var progressModifier: ViewModifier
+        public var showTitle: Bool
+        public var titleColor: Color
+        public var titleFont: Font
+        public var titleModifier: ViewModifier
+
+        public static var standard = CircularProgressBar.Style()
     }
 }
 
 struct CircularProgressBar_Previews: PreviewProvider {
     
     static var previews: some View {
-        
-        var swedishStyle = CircularProgressBarStyle.standard
-        swedishStyle.backgroundColor = .blue
-        swedishStyle.strokeColor = .yellow
-        swedishStyle.strokeWidth = 5
-        swedishStyle.progressColor = .yellow
-        swedishStyle.progressWidth = 15
-        swedishStyle.progressModifier = { $0.shadow(.elevated).any() }
-        
-        var noTextStyle = CircularProgressBarStyle.standard
-        noTextStyle.backgroundColor = .white
-        noTextStyle.strokeColor = .clear
-        noTextStyle.progressColor = .black
-        noTextStyle.showTitle = false
-        
-        return VStack(spacing: 30) {
+        VStack(spacing: 30) {
             CircularProgressBar(
-                progress: .constant(0.1),
+                progress: 0.1,
                 decimals: 2)
-            
+
             CircularProgressBar(
-                progress: .constant(0.256),
+                progress: 0.256,
                 decimals: 0,
                 startAngle: 20,
-                style: swedishStyle)
+                style: .swedish)
             
             CircularProgressBar(
-                progress: .constant(0.8),
-                style: noTextStyle)
+                progress: 0.8,
+                style: .noText)
         }
         .background(Color.green.edgesIgnoringSafeArea(.all))
     }
+}
+
+fileprivate extension CircularProgressBar.Style {
+
+    static var swedish: CircularProgressBar.Style = {
+        var style = CircularProgressBar.Style.standard
+        style.backgroundColor = .blue
+        style.strokeColor = .yellow
+        style.strokeWidth = 5
+        style.progressColor = .yellow
+        style.progressWidth = 15
+        style.progressModifier = { $0.shadow(.elevated).any() }
+        style.titleColor = .yellow
+        style.titleFont = .title.bold()
+        style.titleModifier = { $0.shadow(.elevated).any() }
+        return style
+    }()
+
+    static var noText: CircularProgressBar.Style = {
+        var style = CircularProgressBar.Style.standard
+        style.backgroundColor = .white
+        style.strokeColor = .clear
+        style.progressColor = .black
+        style.showTitle = false
+        return style
+    }()
 }
