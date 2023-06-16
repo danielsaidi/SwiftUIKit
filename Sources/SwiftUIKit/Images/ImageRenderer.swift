7654654ed0eca4a1b29692d755c072ecceaa3894
Maxim Piddubniak <intmaxfactor@gmail.com>
2023-06-16 23:45:42 +0300

    remove size, calculate size from content's view

--- Sources/SwiftUIKit/Images/ImageRenderer.swift
+++ Sources/SwiftUIKit/Images/ImageRenderer.swift
@@ -15,31 +15,25 @@ import UIKit
  which is only available in iOS 16.0, macOS 13.0, tvOS 16.0,
  and watchOS 9.0.
  You can use this renderer in the same way as the native one.
- The only difference is that you have to provide a size when
- you create an instance, since the rendering requires a size.
  You can also define a resolution scale directly in `init`.
  */
 public class ImageRenderer<Content: View> {
     @MainActor
     public init(content: Content) {
         self.content = content
-        self.size = UIScreen.main.bounds.size
         self.scale = UIScreen.main.scale
     }
 
     @MainActor
     public init(
         content: Content,
-        size: CGSize,
         scale: CGFloat? = nil
     ) {
         self.content = content
-        self.size = size
         self.scale = scale ?? UIScreen.main.scale
     }
 
     private let content: Content
-    private let size: CGSize
     @MainActor
     public var scale: CGFloat
 
@@ -49,12 +43,12 @@ public class ImageRenderer<Content: View> {
             let renderer = SwiftUI.ImageRenderer(content: content)
             return renderer.uiImage
         } else {
-            let window = UIWindow(frame: CGRect(origin: .zero, size: size))
-            let hosting = UIHostingController(rootView: content)
-            hosting.view.frame = window.frame
-            window.addSubview(hosting.view)
+            let hostingController = UIHostingController(rootView: content)
+            hostingController.view.frame = UIScreen.main.bounds
+            let window = UIWindow(frame: hostingController.view.frame)
+            window.rootViewController = hostingController
             window.makeKeyAndVisible()
-            return hosting.view.renderedImage(withScale: scale)
+            return hostingController.view.renderedImage(withScale: scale)
         }
     }
 }
